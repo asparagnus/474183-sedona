a22606b157dbf248c03d3ec51bb8648374c5595f
asparagnus <natasha@softdeveloper.ru>
2017-09-12 16:50:35 +0300

    Правки 2

--- js/buttons.js
+++ js/buttons.js
@@ -1,12 +1,5 @@
 var link = document.querySelector(".buttons_serch");
 var popup = document.querySelector(".form");
-var close = document.querySelector(".form-hide");
-
 link.addEventListener("click", function () {
     popup.classList.toggle("form-hide");
-});
-
-close.addEventListener("click", function () {
-    popup.preventDefault();
-    popup.classList.remove("form");
 });
